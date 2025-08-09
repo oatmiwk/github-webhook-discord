b3328aeba338fc0169a042c4c03e83f7933e1a50
Oats <64658473+oatmiwk@users.noreply.github.com>
2025-08-09 19:52:53 -0400

    feat: add configuration file for colours

---
+++ bot/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  prideColors: {
+    isEnabled: false, // Set to true to enable pride colors
+    colors: [
+      0xff5a36, // Red
+      0xffb400, // Orange
+      0xffe600, // Yellow
+      0x26de81, // Green
+      0x3498ff, // Blue
+      0xa259ff, // Purple
+      0xffffff, // White
+      0xf5a9b8, // Pink
+      0x5bcefa, // Light Blue
+      0x836953, // Brown
+      0x1d1c1a, // Black
+    ],
+  },
+  regularColors: {
+    isEnabled: false, // Set to true to enable regular colors
+    colors: 0x3498ff,
+  },
+  pinkOverload: {
+    isEnabled: true, // Set to true to enable pink overload
+    colors: [0xffc0cb, 0xff69b4, 0xff1493, 0xdb7093, 0xc71585],
+  },
+};
