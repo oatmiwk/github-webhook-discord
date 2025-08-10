4d7e85d54c54f981673bd12128cb20108d3a43ba
Oats <64658473+oatmiwk@users.noreply.github.com>
2025-08-09 20:18:05 -0400

    chore: changes to the config file

--- bot/config/config.js
+++ bot/config/config.js
@@ -1,26 +1,17 @@
 module.exports = {
+  regularColors: {
+    isEnabled: true, // Set to true to enable regular colors
+    colors: 0x3498ff,
+  },
   prideColors: {
     isEnabled: false, // Set to true to enable pride colors
     colors: [
-      0xff5a36, // Red
-      0xffb400, // Orange
-      0xffe600, // Yellow
-      0x26de81, // Green
-      0x3498ff, // Blue
-      0xa259ff, // Purple
-      0xffffff, // White
-      0xf5a9b8, // Pink
-      0x5bcefa, // Light Blue
-      0x836953, // Brown
-      0x1d1c1a, // Black
+      0xff5a36, 0xffb400, 0xffe600, 0x26de81, 0x3498ff, 0xa259ff, 0xffffff,
+      0xf5a9b8, 0x5bcefa, 0x836953, 0x1d1c1a,
     ],
   },
-  regularColors: {
-    isEnabled: false, // Set to true to enable regular colors
-    colors: 0x3498ff,
-  },
   pinkOverload: {
-    isEnabled: true, // Set to true to enable pink overload
+    isEnabled: false, // Set to true to enable pink overload
     colors: [0xffc0cb, 0xff69b4, 0xff1493, 0xdb7093, 0xc71585],
   },
 };
